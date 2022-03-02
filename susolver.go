21e7454ccc0130b5fd8ba2ddcedb75f4993ae95c
Simon Anderson <simon.anderson111@gmail.com>
2022-03-02 11:50:06 +1100

    Initial commit

---
+++ susolver.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// Test data:
+// var puzzleInput = "2,8,x,4,5,x,6,x,x,x,5,x,6,9,x,x,x,7,x,x,x,1,8,x,x,3,x,9,x,x,x,x,4,1,x,x,7,2,x,9,x,1,x,5,6,x,x,4,7,x,x,x,x,2,x,6,x,x,4,9,x,x,x,8,x,x,x,1,5,x,6,x,x,x,3,x,7,6,x,2,1;"
+
+func main() {
+
+	fmt.Println("Sudoku solver...")
+
+	// Check inputted data for validity
+	// validatePuzzleInput(puzzleInput)
+
+	// Convert
+	// convert to grid: convertToGrid(array)
+
+}
+
+// func convertToGrid(array a)
