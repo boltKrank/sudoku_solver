52379aa10f9b9def01c8eab7ff5ad096f67968dc
Simon Anderson <simon.anderson111@gmail.com>
2022-03-07 16:21:37 +1100

    section generator completed

--- susolver.go
+++ susolver.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+// TODO: Make pointers for each struct, make an array of such pointers, and parse those points
+
 // Test data:
 
 var puzzleInput = "2,8,0,4,5,0,6,0,0,0,5,0,6,9,0,0,0,7,0,0,0,1,8,0,0,3,0,9,0,0,0,0,4,1,0,0,7,2,0,9,0,1,0,5,6,0,0,4,7,0,0,0,0,2,0,6,0,0,4,9,0,0,0,8,0,0,0,1,5,0,6,0,0,0,3,0,7,6,0,2,1"
@@ -14,6 +16,9 @@ var puzzleInput = "2,8,0,4,5,0,6,0,0,0,5,0,6,9,0,0,0,7,0,0,0,1,8,0,0,3,0,9,0,0,0
 const width_size = 9
 const height_size = 9
 const section_size = 9
+const row_type = 1
+const column_type = 2
+const section_type = 3
 
 // Structs:
 
@@ -22,7 +27,10 @@ type number struct {
 	column_num  int
 	section_num int
 	finished    bool
-	possibles   []int
+	possibles   []string
+	// members in row
+	// members in column
+	// members in section
 }
 
 func main() {
@@ -54,7 +62,33 @@ func convertToGrid(puzzleString []string) {
 
 	// Make it an array of number structs
 
+	// Show row (1-9)
+	fmt.Println(showPart(puzzleString, row_type, 1))
+
+	// Show column (1-9)
+	fmt.Println(showPart(puzzleString, column_type, 1))
+
+	// Show section (1-9)
+	fmt.Println(showPart(puzzleString, section_type, 1))
+
 	gridDisplay(puzzleString)
+
+}
+
+//parameters: Array, row, column, section.
+func showPart(puzzleString []string, partType int, partArea int) string {
+
+	var parType = ""
+
+	if partType == row_type {
+		parType = "ROW"
+	} else if partType == column_type {
+		parType = "COLUMN"
+	} else if partType == section_type {
+		parType = "SECTION"
+	}
+
+	return parType
 
 }
 
