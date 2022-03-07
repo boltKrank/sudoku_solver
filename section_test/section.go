844605884648d5a3273e808442654731d9f23afc
Simon Anderson <simon.anderson111@gmail.com>
2022-03-08 08:45:39 +1100

    array coordinates need fixing for struct

--- section_test/section.go
+++ section_test/section.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 var puzzleInput = "2,8,0,4,5,0,6,0,0,0,5,0,6,9,0,0,0,7,0,0,0,1,8,0,0,3,0,9,0,0,0,0,4,1,0,0,7,2,0,9,0,1,0,5,6,0,0,4,7,0,0,0,0,2,0,6,0,0,4,9,0,0,0,8,0,0,0,1,5,0,6,0,0,0,3,0,7,6,0,2,1"
@@ -21,9 +20,9 @@ func main() {
 	var column int = 1
 	var iter int = 0
 
-	numbers := strings.Split(puzzleInput, ",")
+	//numbers := strings.Split(puzzleInput, ",")
 
-	var grid [8][8]number
+	//var grid [8][8]number
 
 	for h := 0; h <= 6; h += 3 {
 
@@ -31,10 +30,10 @@ func main() {
 			for j := 1; j <= 3; j++ {
 				for k := 1; k <= 3; k++ {
 					fmt.Print("[", row, ",", column, "]: ", j+h, "  ")
-					grid[row-1][column-1].row_num = row - 1
-					grid[row-1][column-1].column_num = column - 1
-					grid[row-1][column-1].section_num = j + h
-					grid[row-1][column-1].value = numbers[iter]
+					//grid[row-1][column-1].row_num = row - 1
+					//grid[row-1][column-1].column_num = column - 1
+					//grid[row-1][column-1].section_num = j + h
+					//grid[row-1][column-1].value = numbers[iter]
 					iter++
 					row++
 				}
